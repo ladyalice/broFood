fbc2e3e9fa7d2fc76ad18d4c6840d1edb76c6b6a
ladyalice <alicejoyk@gmail.com>
2015-04-16 23:39:19 -0700

    Bro Food Initial Commit
    Your resource for finding bros near you. But seriously, this is just a
    random app.

--- broFood/ViewController.swift
+++ broFood/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func showHello() {
+        let showHello = UIAlertController(title: "Good Man and or Woman", message: "Welcome to Brotopia", preferredStyle: UIAlertControllerStyle.Alert)
+        showHello.addAction(UIAlertAction(title: "Ok!", style: UIAlertActionStyle.Default, handler: nil))
+        presentViewController(showHello, animated: true, completion: nil)
+    }
+    @IBOutlet weak var magictext: UILabel!
+    
+    @IBOutlet weak var foodButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,4 +30,33 @@ class ViewController: UIViewController {
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
